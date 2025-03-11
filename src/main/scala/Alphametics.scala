6c0bcbe6f078971c914e42b9cf17730030890c43
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-11 15:44:15 +0100

    refactoring

--- src/main/scala/Alphametics.scala
+++ src/main/scala/Alphametics.scala
@@ -1,8 +1,8 @@
 object Alphametics:
-   def solve(equation: String): Option[Map[Char, Long]] =
+   def solve(equation: String): Option[Map[Char, Int]] =
       val (left, right) = parseEquation(equation)
       val uniqueLetters = (left.flatMap(_.toSet) ++ right.toSet).toSet.mkString
-      backtrack(uniqueLetters, left, right, Map.empty, Set.empty)
+      backtrack(uniqueLetters, left, right, Map.empty, Seq.empty)
 
    private def parseEquation(equation: String): (Seq[String], String) =
       val parts = equation.filterNot(_.isSpaceChar).split("==").toSeq
@@ -13,21 +13,22 @@ object Alphametics:
        letters: String,
        left: Seq[String],
        right: String,
-       currentMapping: Map[Char, Long],
-       usedDigits: Set[Long]): Option[Map[Char, Long]] =
-      if letters.isEmpty then if isValidMapping(currentMapping, left, right) then Some(currentMapping) else None
+       currentMapping: Map[Char, Int],
+       usedDigits: Seq[Int]): Option[Map[Char, Int]] =
+      if letters.isEmpty then
+         if isValidMapping(currentMapping, left, right) then Some(currentMapping)
+         else None
       else
          val letter = letters.head
          (0 to 9)
             .to(LazyList)
             .flatMap { digit =>
-               val digitLong = digit.toLong
-               if isLeadingZero(letter, left, right, digit) || usedDigits.contains(digitLong) then None
-               else backtrack(letters.tail, left, right, currentMapping + (letter -> digitLong), usedDigits + digitLong)
+               if isLeadingZero(letter, left, right, digit) || usedDigits.contains(digit) then None
+               else backtrack(letters.tail, left, right, currentMapping + (letter -> digit), usedDigits.appended(digit))
             }
             .headOption
 
-   private def isValidMapping(mapping: Map[Char, Long], left: Seq[String], right: String): Boolean =
+   private def isValidMapping(mapping: Map[Char, Int], left: Seq[String], right: String): Boolean =
       val leftSum = left.map(word => word.map(mapping).mkString("").toLong).sum
       val rightValue = right.map(mapping).mkString("").toLong
       leftSum == rightValue
