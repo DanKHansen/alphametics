f4bc13558aaf838a28b32959715cb3faf93bdeb9
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-11 15:33:21 +0100

    refactoring

--- src/main/scala/Alphametics.scala
+++ src/main/scala/Alphametics.scala
@@ -1,17 +1,17 @@
 object Alphametics:
    def solve(equation: String): Option[Map[Char, Long]] =
       val (left, right) = parseEquation(equation)
-      val uniqueLetters = (left.flatMap(_.toSet) ++ right.toSet).toSet.toList
+      val uniqueLetters = (left.flatMap(_.toSet) ++ right.toSet).toSet.mkString
       backtrack(uniqueLetters, left, right, Map.empty, Set.empty)
 
-   private def parseEquation(equation: String): (Array[String], String) =
-      val parts = equation.split("==").map(_.trim)
-      val left = parts(0).split("\\+").map(_.trim)
+   private def parseEquation(equation: String): (Seq[String], String) =
+      val parts = equation.filterNot(_.isSpaceChar).split("==").toSeq
+      val left = parts.head.filterNot(_.isSpaceChar).split("\\+").toSeq
       (left, parts(1))
 
    private def backtrack(
-       letters: List[Char],
-       left: Array[String],
+       letters: String,
+       left: Seq[String],
        right: String,
        currentMapping: Map[Char, Long],
        usedDigits: Set[Long]): Option[Map[Char, Long]] =
@@ -27,11 +27,11 @@ object Alphametics:
             }
             .headOption
 
-   private def isValidMapping(mapping: Map[Char, Long], left: Array[String], right: String): Boolean =
+   private def isValidMapping(mapping: Map[Char, Long], left: Seq[String], right: String): Boolean =
       val leftSum = left.map(word => word.map(mapping).mkString("").toLong).sum
       val rightValue = right.map(mapping).mkString("").toLong
       leftSum == rightValue
 
-   private def isLeadingZero(letter: Char, left: Array[String], right: String, digit: Int): Boolean =
+   private def isLeadingZero(letter: Char, left: Seq[String], right: String, digit: Int): Boolean =
       val isFirstLetter = (word: String) => word.head == letter
       digit == 0 && (left.exists(isFirstLetter) || right.headOption.contains(letter))
